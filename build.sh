c3c18d5386e4b7aaca0e4c579f5a3448648bfb43
KagurazakaNyaa <i@kagurazakanyaa.com>
2021-03-23 04:00:24 +0000

    自动更新版本

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+version=$(curl --silent "https://api.github.com/repos/iTXTech/mirai-console-loader/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+currentversion=$(cat currentversion)
+echo $version > currentversion
+if [[ "$currentversion" == "$version" ]]; then
+    exit
+fi
+sed -i 's/^ENV MCL_VERSION.*$/ENV MCL_VERSION '$version'/i' Dockerfile
+date > timestamp
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add timestamp
+git add currentversion
+git commit -a -m "Auto Update to screeps "$version
+git tag -f $version
+git tag -f latest
+git push
+git push origin --tags -f
